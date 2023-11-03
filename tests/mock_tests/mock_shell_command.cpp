dc0e29b4011d5c782e4146ee458cb50ecd4628eb
Lawrence Lee <lawlee@microsoft.com>
2023-11-02 22:03:30 -0700

    [202205][teamd]: Clean teamd process if LAG creation fails (#2888) (#2932)
    What I did
    
    202205 cherry-pick for [teamd]: Clean teamd process if LAG creation fails #2888
    During LAG creation, if the teamd process fails for any reason, cleanup any leftover teamd processes for the LAG alias
    Call kill directly in removeLag instead of using teamd -k
    
    Why I did it
    
    If the teamd process times out for any reason, subsequent attempts to create a LAG may fail if there is an orphaned process left running
    As detailed in Removal of LAG with no members takes around 1 sec on 202012  sonic-buildimage#8071, team -k may block for some time.
    
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/mock_tests/mock_shell_command.cpp
+++ tests/mock_tests/mock_shell_command.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include <vector>
 
+/* Override this pointer for custom behavior */
+int (*callback)(const std::string &cmd, std::string &stdout) = nullptr;
+
 int mockCmdReturn = 0;
 std::string mockCmdStdcout = "";
 std::vector<std::string> mockCallArgs;
@@ -8,8 +11,15 @@ std::vector<std::string> mockCallArgs;
 namespace swss {
     int exec(const std::string &cmd, std::string &stdout)
     {
-        mockCallArgs.push_back(cmd);
-        stdout = mockCmdStdcout;
-        return mockCmdReturn;
+        if (callback != nullptr)
+        {
+            return callback(cmd, stdout);
+        }
+        else
+        {
+            mockCallArgs.push_back(cmd);
+            stdout = mockCmdStdcout;
+            return mockCmdReturn;
+        }
     }
 }
