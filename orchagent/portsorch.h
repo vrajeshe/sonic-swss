ab7b206a5f1e465c2018edc5c0ea545243cc04c4
Nazarii Hnydyn <nazariig@nvidia.com>
2023-11-10 21:59:10 +0200

    [portsorch]: Implement port PFC asym capability check (#2942)
    * [portsorch]: Implement port PFC asym capability check.

--- orchagent/portsorch.h
+++ orchagent/portsorch.h
@@ -16,6 +16,7 @@
 #include "flexcounterorch.h"
 #include "events.h"
 
+#include "port/port_capabilities.h"
 #include "port/porthlpr.h"
 
 #define FCS_LEN 4
@@ -495,6 +496,9 @@ private:
 private:
     // Port config aggregator
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> m_portConfigMap;
+
+    // Port OA capabilities
+    PortCapabilities m_portCap;
 
     // Port OA helper
     PortHelper m_portHlpr;
