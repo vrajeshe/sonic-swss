5075e660fb56c3447da9118f3820e618687b8889
Bobby McGonigle <bobbymcgonigle@arista.com>
2024-05-23 10:21:26 -0700

    Parse subport value added to config_db (#3161)
    * Parse subport value added to config_db

--- orchagent/port/portschema.h
+++ orchagent/port/portschema.h
@@ -86,3 +86,4 @@
 #define PORT_ROLE                "role"
 #define PORT_ADMIN_STATUS        "admin_status"
 #define PORT_DESCRIPTION         "description"
+#define PORT_SUBPORT             "subport"
