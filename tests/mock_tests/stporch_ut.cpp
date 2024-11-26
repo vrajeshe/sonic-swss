c2136d328aae3d451fbbe71c1a30cd24a83d0e20
Divya Chandralekha <divya.chandralekha@broadcom.com>
2024-11-25 21:09:16 -0800

    Fix errors

--- tests/mock_tests/stporch_ut.cpp
+++ tests/mock_tests/stporch_ut.cpp
@@ -49,14 +49,14 @@ TEST_F(StpOrchTest, TestAddStpPort) {
     sai_object_id_t stp_port_id = 67890;
 
     sai_attribute_t attrs[2] = {};
-    attrs[0].id = SAI_STP_PORT_ATTR_BRIDGE_PORT_ID;
+    attrs[0].id = SAI_STP_PORT_ATTR_BRIDGE_PORT;
     attrs[0].value.oid = port.m_port_id;
 
     attrs[1].id = SAI_STP_PORT_ATTR_STATE;
     attrs[1].value.u32 = SAI_STP_PORT_STATE_FORWARDING;
 
-    EXPECT_CALL(*mock_sai_stp, create_stp_port(_, _, 2, _))
-        .WillOnce(::testing::DoAll(::testing::SetArgPointee<0>(stp_port_id),
+    EXPECT_CALL(*mock_sai_stp, 
+        create_stp_port(_, _, 2, _)).WillOnce(::testing::DoAll(::testing::SetArgPointee<0>(stp_port_id),
                                     ::testing::Return(SAI_STATUS_SUCCESS)));
 
     bool result = stpOrch->create_stp_port(stp_port_id);
@@ -67,28 +67,11 @@ TEST_F(StpOrchTest, TestAddStpPort) {
 TEST_F(StpOrchTest, TestRemoveStpPort) {
     sai_object_id_t stp_port_id = 67890;
 
-    EXPECT_CALL(*mock_sai_stp, remove_stp_port(stp_port_id))
-        .WillOnce(::testing::Return(SAI_STATUS_SUCCESS));
+    EXPECT_CALL(*mock_sai_stp, 
+        remove_stp_port(stp_port_id)).WillOnce(::testing::Return(SAI_STATUS_SUCCESS));
   
     bool result = stpOrch->removeStpPort(stp_port_id);
 
-    EXPECT_TRUE(result);
-}
-
-TEST_F(StpOrchTest, TestUpdateMaxStpInstance) {
-    uint32_t max_stp_instance = 10;
-    sai_object_id_t stp_instance_id;
-
-    sai_attribute_t attr;
-    attr.id = SAI_SWITCH_ATTR_MAX_STP_INSTANCE;
-    attr.value.u32 = max_stp_instance;
-
-    EXPECT_CALL(*mock_sai_stp, create_stp_instance(_, _, _, _))
-        .WillOnce(::testing::Return(SAI_STATUS_SUCCESS));
-
-    bool result = stpOrch->updateMaxStpInstance(max_stp_instance);
-
-    // Assert
     EXPECT_TRUE(result);
 }
 
